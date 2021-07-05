cd83cfce9cecda7ea3f7deb780b1e9b4edfa18c0
root <root@Revision-PC.localdomain>
2021-07-05 12:26:17 -0300

    ponto de partida

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('posts');
+});
+
+Route::get('post/{post}', function ($slug) {
+    $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+
+    return view ('post', [
+        'post' => $post
+    ]);
 });
